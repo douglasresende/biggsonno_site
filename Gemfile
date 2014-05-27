b474782f0f4f4b618d519ce39b857f433a23565b
Douglas Camargo <douglasresende@maxstudio.com.br>
2014-05-27 14:28:00 -0300

    Fixing assets (y)

--- Gemfile
+++ Gemfile
@@ -13,12 +13,13 @@ group :production do
 end
 
 group :assets do
-  gem 'sass-rails', '~> 4.0.0'
+  gem 'sass-rails'
   gem 'coffee-rails', '~> 4.0.0'
-  gem 'uglifier', '>= 1.3.0'
+  gem 'uglifier'
   gem 'jquery-rails'
   gem 'bootstrap-sass', '>= 3.0.0.0'
   gem 'therubyracer', platforms: :ruby
+  gem 'compass-rails'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module BiggsonnoSite
     config.autoload_paths += %W( #{config.root}/app/resources/**/* )
     # config.assets.precompile += ['biggsonno/biggsonno.js']
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.precompile += %w(application.css.scss)
+    config.assets.precompile += %w(biggsonno.css.scss)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
